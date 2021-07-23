45cf50a928b7cd344d8305c1463078549e953cc7
unknown <roomacarthur@gmail.com>
2021-07-23 08:35:52 +0100

    initial commit, imported chinook DB and tested commands for fetching data with python

---
+++ sql-psycopg2.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+
+# connect to "chinook" database
+
+connection = psycopg2.connect(database="chinook", user="postgres", password="london91")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - Select all records from the "Artist" table.
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - Select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - Select only "Queen" form the "Artist" Table. 
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+
+# Query 4 - Select only "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s',[51])
+
+# Query 5 - Select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51]) 
+
+# Query 6 - Select all tracks where the composer is "Queen" from the "Track" table. 
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+
+# fetch the results (multiple), displays a tuple.
+results = cursor.fetchall()
+
+# fetch the results (single)
+# results = cursor.fetchone()
+
+#close connection
+
+connection.close()
+
+#print results
+
+for result in results:
+    print(result)
