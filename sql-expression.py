970d933b206fcb558596f0e0b552bedf8fa919c7
unknown <roomacarthur@gmail.com>
2021-07-23 10:24:38 +0100

    ran basic queries via sclalchemy

--- sql-expression.py
+++ sql-expression.py
@@ -43,7 +43,22 @@ track_table = Table(
 
 with db.connect() as connection:
     # Query 1 - select all records from the "Artist" table:
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 - Select only "Queen" from the "Artist" table.
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 - Select only by "ArtistId" #51 from the "Artist" table.
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 - Select only the albums with "ArtistId" #51 on the "Album" table.
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - Select all tracks where composer is 'Queen' from the "track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
