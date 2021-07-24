12b89f29d4fe6d624f610b94dcd22895b68c83d2
unknown <roomacarthur@gmail.com>
2021-07-24 11:06:18 +0100

    unittest test cases for new files

---
+++ test_students.py
@@ -0,0 +1,37 @@
+import unittest
+from student import Student
+
+class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+    
+    def tearDown(self):
+        print("tearDown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+    
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
+
+if __name__ == "__main__":
+    unittest.main()
