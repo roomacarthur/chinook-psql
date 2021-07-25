274e279e2acad23c3662078cc8faa1b2ccc88594
unknown <roomacarthur@gmail.com>
2021-07-25 08:38:14 +0100

    unittest students files

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 class Student:
     """ A Student class as a basee for method testing """
@@ -19,3 +20,14 @@ class Student:
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days = days)
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
